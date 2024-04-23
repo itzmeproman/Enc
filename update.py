8d87692752351990c9b3b2a45413051f6529f7eb
itzmeproman <atulpandey3030@gmail.com>
2024-04-23 15:24:55 +0530

    Update update.py

--- update.py
+++ update.py
@@ -15,7 +15,7 @@ try:
     UPSTREAM_REPO = config(
         "UPSTREAM_REPO",
         default="https://github.com/itzmeproman/enc")
-    UPSTREAM_BRANCH = config("UPSTREAM_BRANCH", default="720p")
+    UPSTREAM_BRANCH = config("UPSTREAM_BRANCH", default="Ongoing-480")
 
 except Exception:
     print("Environment vars Missing")
