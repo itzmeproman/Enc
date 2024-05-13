44c4d6152091f10a8bb908f0bd9ad0c3c93e86d9
itzmeproman <atulpandey3030@gmail.com>
2024-05-13 19:54:15 +0530

    Update update.py

--- update.py
+++ update.py
@@ -15,7 +15,7 @@ try:
     UPSTREAM_REPO = config(
         "UPSTREAM_REPO",
         default="https://github.com/itzmeproman/enc")
-    UPSTREAM_BRANCH = config("UPSTREAM_BRANCH", default="main")
+    UPSTREAM_BRANCH = config("UPSTREAM_BRANCH", default="public")
 
 except Exception:
     print("Environment vars Missing")
